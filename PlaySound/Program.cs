be7ea86719959e844efda1fc3e87d82b30c0379d
qiyubrother <13240137763@163.com>
2018-12-23 22:04:36 +0800

    .

--- PlaySound/Program.cs
+++ PlaySound/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PlaySound
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Player.Instance.Add("demo", "demoMp3.wav");
+            new Task(() =>
+            {
+                Console.WriteLine("Playing in background thread.");
+                Player.Instance.Play("demo");
+            }).Start();
+
+            Console.WriteLine("Play sound.");
+            Console.ReadLine();
         }
     }
 }
